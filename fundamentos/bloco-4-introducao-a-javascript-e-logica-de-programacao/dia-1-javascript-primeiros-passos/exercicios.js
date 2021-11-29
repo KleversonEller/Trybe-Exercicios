e59b9e527139f5136cefba4f29eac2b966848eff
Kleverson Eller <kleverson.a16@gmail.com>
2021-11-29 15:53:07 -0300

    utilizando os cada operação aritmética básica

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios.js
@@ -0,0 +1,10 @@
+const a = 10;
+const b = 5;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a / b);
+console.log(a * b);
+console.log(a % b);
+
+// exercícios utilizando os cada operação aritmética básica.
