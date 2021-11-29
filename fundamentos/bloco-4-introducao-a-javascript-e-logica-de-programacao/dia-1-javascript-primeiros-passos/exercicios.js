8dd39e07d832e0cefaecc5d98e61767411775e18
Kleverson Eller <kleverson.a16@gmail.com>
2021-11-29 16:05:33 -0300

    utilizando condicoes if/else e operadores logicos

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios.js
@@ -8,3 +8,28 @@ console.log(a * b);
 console.log(a % b);
 
 // exercícios utilizando os cada operação aritmética básica.
+
+const primeiroNumero = 25;
+const segundoNumero = 35;
+
+if (primeiroNumero > segundoNumero) {
+    console.log(primeiroNumero);
+} else {
+    console.log(segundoNumero);
+}
+
+// exercícios que retorna o maior numero dentre dois números.
+
+const primeiroNumeros = 9;
+const segundoNumeros = 48;
+const terceiroNumeros = 58;
+
+if (primeiroNumeros > segundoNumeros && primeiroNumeros > terceiroNumeros) {
+    console.log(primeiroNumeros);
+} else if (segundoNumeros > primeiroNumeros && segundoNumeros > terceiroNumeros) {
+    console.log(segundoNumeros);
+} else {
+    console.log(terceiroNumeros);
+}
+
+// exercícios que retorna o maior numero dentre tres números.
