778b5cae0569f7425e6b552011037e18126a27b6
Kleverson Eller <kleverson.a16@gmail.com>
2021-11-30 19:19:51 -0300

    imprimir soma dos valores do array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -5,4 +5,11 @@ for (const imprimir of numbers) {
 }
 
 // exercício imprimindo valores de um array
+let resultado = 0;
+for ( let index = 0; index < numbers.length; index += 1) {
+    resultado = resultado + numbers[index];
+}
 
+console.log(resultado);
+
+// exercício imprimir a soma dos valores no array
