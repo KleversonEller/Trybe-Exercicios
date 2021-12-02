7e0e1f2d9a0d2db125afa6d98204ae1f7f6541da
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-02 00:43:37 -0300

    imprime a maior palavra do array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/exercicios.js
@@ -12,7 +12,24 @@ let word = 'binho';
 let wordInverso = "";
 
 for (let inverte in word){
-    let letra = (word.length - inverte) - 1;
+    let letra = (word.length - inverte) -1;
     wordInverso = wordInverso + word[letra];
 }
 console.log(wordInverso);
+
+// Imprime a variável ao contrario
+
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let comparador = 0;
+let resultado2 = null;
+let cont = 0;
+for(let palavra of array){
+   cont = palavra.length
+    if (comparador < cont){
+        comparador = cont;
+        resultado2 = palavra;
+    }
+}
+console.log(resultado2);
+
+// Imprime qual a maior palavra dentro do array
