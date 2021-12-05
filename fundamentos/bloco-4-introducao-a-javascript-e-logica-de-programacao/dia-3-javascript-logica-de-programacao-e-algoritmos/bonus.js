c758bdc6064e2080662684c52d293fa0c9d54977
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-04 23:07:56 -0300

    Bonus - imprime um triangulo invertido de '*' dado um valor

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -17,3 +17,18 @@ for (let index = 1; index <= valor; index += 1){
     resultado2 += "*";
         console.log(resultado2);
 }
+
+// Imprime um triângulo com tamanho equivalentes a um valor dado 
+
+let resultado3 = [];
+let valor3 = 5;
+
+for( index = 1; index <= valor3; index += 1){
+    resultado3.push(" ");
+}
+for(index = 1; index <= valor3; index += 1){
+    resultado3[(resultado3.length)- index] = "*";
+    console.log(resultado3.join(''));
+}
+
+// Imprime um triângulo invertido com tamanho equivalentes a um valor dado 
