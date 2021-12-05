d60058f90045419f402401577b1415d2354a79cc
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-04 22:39:38 -0300

    Bonus - imprime um quadrado de '*' dado um valor

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -0,0 +1,11 @@
+let resultado = "";
+let valor = 5;
+
+for (let index = 1; index <= valor; index += 1){
+    resultado += "*";
+}
+for(let index = 1; index <= valor; index += 1){
+    console.log(resultado);
+}
+
+// Imprime um quadrado com tamanho equivalentes a um valor dado 
