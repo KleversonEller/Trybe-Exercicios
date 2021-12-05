8ce1e11ba2756275291ad7ab1cd0b4afa4866b5e
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-04 22:44:56 -0300

    Bonus - imprime um triangulo de '*' dado um valor

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/bonus.js
@@ -9,3 +9,11 @@ for(let index = 1; index <= valor; index += 1){
 }
 
 // Imprime um quadrado com tamanho equivalentes a um valor dado 
+
+let resultado2 = "";
+let valor2 = 5;
+
+for (let index = 1; index <= valor; index += 1){
+    resultado2 += "*";
+        console.log(resultado2);
+}
