637402c512a2aac56c636820dc53255d815da589
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-09 17:56:27 -0300

    Exercicio 2 - retorna o indice do maior valor do array

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicios/objetos--for/funcoes.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicios/objetos--for/funcoes.js
@@ -11,3 +11,18 @@ function palindromo(palavra) {
 }
 
 // Function que verifica se uma palavra é um palíndromo
+
+function maiorValor(array) {
+  let comparador = null;
+  for (let valor in array) {
+    if (comparador === null) {
+      comparador = valor;
+    }
+    if (array[comparador] < array[valor]) {
+      comparador = valor;
+    }
+  }
+  return comparador;
+}
+
+// Function que retorna o índice do maior valor da array
