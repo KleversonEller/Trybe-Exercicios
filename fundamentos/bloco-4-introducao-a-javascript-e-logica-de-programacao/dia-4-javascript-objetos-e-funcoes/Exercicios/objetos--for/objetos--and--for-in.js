9b2d6cd9a3b7ef4c470fc9daaab0286abc567627
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-09 16:43:09 -0300

    Exercicio 6 - imprime a frase usando os valores do objeto

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicios/objetos--for/objetos--and--for-in.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercicios/objetos--for/objetos--and--for-in.js
@@ -44,3 +44,27 @@ for (let valor in info) {
 }
 
 // Criar um novo objeto e imprimir os valores dos dois objetos no formato solicitado
+
+let leitor = {
+  nome: "Julia",
+  sobrenome: "Pessoa",
+  idade: 21,
+  livrosFavoritos: [
+    {
+      titulo: "O Pior Dia de Todos",
+      autor: "Daniela Kopsch",
+      editora: "Tordesilhas",
+    },
+  ],
+};
+
+console.log(
+  "O livro favorito de " +
+    leitor.nome +
+    " " +
+    leitor.sobrenome +
+    " se chama " +
+    leitor.livrosFavoritos[0]["titulo"]
+);
+
+// Imprime a frase solicitada utilizando os valores do objeto
