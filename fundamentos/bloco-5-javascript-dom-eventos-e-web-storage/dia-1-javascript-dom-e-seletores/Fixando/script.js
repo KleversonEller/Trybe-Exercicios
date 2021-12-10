c929dd2a5981af3dadd09dd3fb21b36ccd60bf4c
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-10 19:15:50 -0300

    Fixando 2 - utilizando 'querySelector'

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/Fixando/script.js
@@ -0,0 +1,15 @@
+document.querySelector("#header-container").style.backgroundColor = "green";
+document.querySelector(".emergency-tasks").style.backgroundColor = "pink";
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "yellow";
+document.querySelectorAll(".emergency-tasks div h3")[0].style.backgroundColor =
+  "purple";
+document.querySelectorAll(".emergency-tasks div h3")[1].style.backgroundColor =
+  "purple";
+document.querySelectorAll(
+  ".no-emergency-tasks div h3"
+)[0].style.backgroundColor = "black";
+document.querySelectorAll(
+  ".no-emergency-tasks div h3"
+)[1].style.backgroundColor = "black";
+document.querySelector("#footer-container").style.backgroundColor =
+  "rgb(2, 65, 2)";
