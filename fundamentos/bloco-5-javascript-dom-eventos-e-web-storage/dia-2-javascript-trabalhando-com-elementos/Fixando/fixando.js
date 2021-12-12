6358494ee4654ede8d22201e32a952a3dc3ab2dd
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-12 17:55:45 -0300

    Add anotaçoes de pendencias do fixando e começando os exercicios

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/Fixando/fixando.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/Fixando/fixando.js
@@ -29,13 +29,46 @@ document.querySelector("#primeiroFilhoDoFilho").appendChild(criando3);
 // Fixando parte III
 
 let pai = document.querySelector("#pai");
-let filho = document.querySelector("#elementoOndeVoceEsta");
+let filho = document.querySelector("#elementoOndeVoceEsta").childNodes;
 let filho2 = document.querySelector("#primeiroFilhoDoFilho");
-let remover = document.querySelectorAll("section");
+let remover = document.getElementById("pai").childNodes;
 
 for (let index = 0; index < remover.length; index += 1) {
   let elemento = remover[index];
-  if (elemento !== filho && elemento !== pai && elemento !== filho2) {
+  if (elemento.id != "elementoOndeVoceEsta") {
+    remover[index].remove();
+  }
+}
+
+for (let index = 0; index < filho.length; index += 1) {
+  let elemento = remover[index];
+  if (elemento != filho2) {
     elemento.remove();
   }
 }
+
+// FIXME Código esta funcionando porem ta dando erro, a propriedade 'REMOVE' nao consegue ler valores indefinidos
+
+// const pai = document.getElementById("pai").childNodes;
+
+// for (let index = 0; index < pai.length - 1; index += 1) {
+//   const elemento = pai[index];
+//   console.log(elemento.id);
+//   if (elemento.id !== "elementoOndeVoceEsta") {
+//     elemento.remove();
+//   }
+// }
+// console.log(pai);
+
+// const pai2 = document.getElementById("pai").childNodes;
+
+// for (let index = pai2.length - 1; index >= 0; index -= 1) {
+//   const elemento = pai2[index];
+//   console.log(elemento.id);
+//   if (elemento.id !== "elementoOndeVoceEsta") {
+//     elemento.remove();
+//   }
+// }
+// console.log(pai2);
+
+// TODO verificar o porque o codigo acima funciona com for inverso mas nao funciona com o for linear
