5c72d872b97fae282acca696e99f36ac73b38ccc
Kleverson Eller <kleverson.a16@gmail.com>
2021-12-13 00:43:07 -0300

    Exercicios 1 - criando os dias do calendario

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/Exercicios/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/Exercicios/script.js
@@ -22,3 +22,24 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+const dezDaysList = [
+  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+];
+for (let dias of dezDaysList) {
+  let cria = document.createElement("li");
+  cria.innerText = dias;
+  cria.classList.add("day");
+  if (dias == 25) {
+    cria.classList.add("holiday", "friday");
+  } else if (dias == 24 || dias == 31) {
+    cria.classList.add("holiday");
+  } else if (dias == 4 || dias == 11 || dias == 18) {
+    cria.classList.add("friday");
+  }
+
+  document.querySelector("#days").appendChild(cria);
+}
+
+// Cria dinamicamente os elementos dos dias do calendário com os dias e suas classes
