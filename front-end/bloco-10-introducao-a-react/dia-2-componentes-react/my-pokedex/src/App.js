f1ec406a22fa224dbfda08f8d611534fbe433fe1
Kleverson Eller <kleverson.a16@gmail.com>
2022-02-17 22:31:31 -0300

    Implementando o estilo CSS

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <main>
-        <h1>Pokedex</h1>
+        <h1>POKEDEX</h1>
         <Pokedex />
       </main>
     );
--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/pokemon.jsx
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/my-pokedex/src/components/pokemon.jsx
@@ -5,15 +5,17 @@ class Pokemon extends Component {
     render () {
         const {catchPokemon} = this.props
         return (
-            <div className='flex-container'>
-                <div>
-                    <p> {catchPokemon.name} </p>
-                    <p> {catchPokemon.type} </p>
-                    <p> {catchPokemon.averageWeight.value}{catchPokemon.averageWeight.measurementUnit}</p>
-                    <a href={catchPokemon.moreInfo} target='_blank'> {catchPokemon.name} Wiki</a>
+            <section>
+                <div className='flex-container'>
+                    <div>
+                        <p> {catchPokemon.name} </p>
+                        <p> {catchPokemon.type} </p>
+                        <p> {catchPokemon.averageWeight.value}{catchPokemon.averageWeight.measurementUnit}</p>
+                        <a href={catchPokemon.moreInfo} target='_blank'> {catchPokemon.name} Wiki</a>
+                    </div>
+                    <img src={catchPokemon.image} alt={`Essa é uma ilustração do ${catchPokemon.name}`} />
                 </div>
-                <img src={catchPokemon.image} alt={`Essa é uma ilustração do ${catchPokemon.name}`} />
-            </div>
+            </section>
         )
     }
 }
